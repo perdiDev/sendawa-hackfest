be36d6f1eb4ba3a520a3f54b70bfbe2fade47332
Fayzul <fayzulhaq0708@gmail.com>
2023-12-29 22:09:08 +0800

    feat: auth provider

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,48 +1,54 @@
-'use client'
-import { createContext, useContext, useEffect, useState, ReactNode } from 'react';
-import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
-import firebase_app from '@/firebase/config';
-
-// Initialize Firebase auth instance
-const auth = getAuth( firebase_app );
+"use client";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "@/firebase/index";
+import Loading from "@/components/ui/Loading";
 
 // Create the authentication context
-export const AuthContext = createContext( {} );
+export const AuthContext = createContext({});
 
 // Custom hook to access the authentication context
-export const useAuthContext = () => useContext( AuthContext );
+export const useAuthContext = () => useContext(AuthContext);
 
 interface AuthContextProviderProps {
   children: ReactNode;
 }
 
-export function AuthContextProvider( { children }: AuthContextProviderProps ): JSX.Element {
+export function AuthContextProvider({
+  children,
+}: AuthContextProviderProps): JSX.Element {
   // Set up state to track the authenticated user and loading status
-  const [ user, setUser ] = useState<User | null>( null );
-  const [ loading, setLoading ] = useState( true );
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
 
-  useEffect( () => {
+  useEffect(() => {
     // Subscribe to the authentication state changes
-    const unsubscribe = onAuthStateChanged( auth, ( user ) => {
-      if ( user ) {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
         // User is signed in
-        setUser( user );
+        setUser(user);
       } else {
         // User is signed out
-        setUser( null );
+        setUser(null);
       }
       // Set loading to false once authentication state is determined
-      setLoading( false );
-    } );
+      setLoading(false);
+    });
 
     // Unsubscribe from the authentication state changes when the component is unmounted
     return () => unsubscribe();
-  }, [] );
+  }, []);
 
   // Provide the authentication context to child components
   return (
     <AuthContext.Provider value={{ user }}>
-      {loading ? <div>Loading...</div> : children}
+      {loading ? <div className="flex justify-center items-center h-screen w-full"><Loading/></div> : children}
     </AuthContext.Provider>
   );
 }
