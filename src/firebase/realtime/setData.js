e2531646b8b156c39cf0bcaa03a47b4fc1b25888
Fayzul <fayzulhaq0708@gmail.com>
2023-12-30 23:40:42 +0800

    chore: delete js

--- src/firebase/realtime/setData.js
+++
@@ -1,7 +0,0 @@
-import { getDatabase, ref, set } from "firebase/database";
-
-export default function setDataRealtime(reference,userId, data) {
-  const db = getDatabase();
-  set(ref(db, `${reference}/${userId}`),data);
-}
-
