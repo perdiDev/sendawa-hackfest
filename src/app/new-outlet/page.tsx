130a8e01e8f75a5e0bca4fddde6bfa4829d2e691
Fayzul <fayzulhaq0708@gmail.com>
2023-12-30 23:42:31 +0800

    feat: add profile and image input

--- src/app/new-outlet/page.tsx
+++ src/app/new-outlet/page.tsx
@@ -2,7 +2,7 @@
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import Form1 from "@/components/Form1";
+import Form1 from "@/components/new-outlet-form/Form";
 
 export default function Page(): JSX.Element {
   const { user } = useAuthContext() as { user: any };
@@ -14,18 +14,20 @@ export default function Page(): JSX.Element {
     }
   }, [user, router]);
 
-  console.log(user)
+  console.log(user);
 
   return (
     <>
       <div className="flex flex-col items-center gap-3 mx-auto px-5">
-        <div className="items-self-center mt-5 flex gap-5 items-baseline"> 
+        <div className="items-self-center mt-5 flex gap-5 items-baseline">
           <h2 className="text-primary-green font-bold text-3xl">
             <span className="text-3xl md:text-4xl font-extrabold">1</span>/3
           </h2>
-          <h3 className="text-lg md:text-xl">Fill all information about your outlet</h3>
+          <h3 className="text-lg md:text-xl">
+            Fill all information about your outlet
+          </h3>
         </div>
-        <Form1 userId={user.uid}/>
+        <Form1 userId={user.uid} />
       </div>
     </>
   );
