dd79243b4db809b593807b0c15706a19bfa53e44
Fayzul <fayzulhaq0708@gmail.com>
2024-01-09 23:31:06 +0800

    refactor: use button comp instead of their own style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,38 @@
 import Link from "next/link";
+import BottomModal from "@/components/BottomModal";
+import { Button } from "@/components/ui/button";
 
 export default function page() {
   return (
     <main className="flex min-h-screen justify-center items-center gap-2 p-24 bg-white">
       <h2>Ini nanti halaman landingpage</h2>
-      <Link href="/signin" className="bg-primary-green px-6 text-center text-white font-semibold rounded-lg py-1.5">
+      <Link
+        href="/signin"
+        className="bg-primary-green px-6 text-center text-white font-semibold rounded-lg py-1.5"
+      >
         <span className="link">SignIn</span>
       </Link>
-      <Link href="/signup" className="bg-primary-green px-6 text-center text-white font-semibold rounded-lg py-1.5">
+      <Link
+        href="/signup"
+        className="bg-primary-green px-6 text-center text-white font-semibold rounded-lg py-1.5"
+      >
         <span className="link">SignUp</span>
       </Link>
+      <BottomModal isOpen={false}>
+        <div className="flex flex-col justify-between h-full gap-10 mt-10">
+          <div>
+            <h1 className="font-semibold text-2xl">Halo ini judul</h1>
+            <p className="text-slate-600">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptate, assumenda.okokokok ook oko kokok oko
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <Button>Paham</Button>
+            <Button variant="disable">Paham</Button>
+          </div>
+        </div>
+      </BottomModal>
     </main>
   );
 }
