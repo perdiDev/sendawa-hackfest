b67bdab0abdfd94afdb1b0386911b6ad7541f84c
Fayzul <fayzulhaq0708@gmail.com>
2024-01-09 23:28:57 +0800

    refactor: use AuthLayout comp

--- src/app/new-outlet/page.tsx
+++ src/app/new-outlet/page.tsx
@@ -1,23 +1,13 @@
-"use client";
+// "use client";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import Form1 from "@/components/new-outlet-form/Form";
+import AuthLayout from "@/components/layouts/AuthLayout";
 
 export default function Page(): JSX.Element {
-  const { user } = useAuthContext() as { user: any };
-  const router = useRouter();
-
-  useEffect(() => {
-    if (user == null) {
-      router.push("/");
-    }
-  }, [user, router]);
-
-  console.log(user);
-
   return (
-    <>
+    <AuthLayout>
       <div className="flex flex-col items-center gap-3 mx-auto px-5">
         <div className="items-self-center mt-5 flex gap-5 items-baseline">
           <h2 className="text-primary-green font-bold text-3xl">
@@ -27,8 +17,8 @@ export default function Page(): JSX.Element {
             Fill all information about your outlet
           </h3>
         </div>
-        <Form1 userId={user.uid} />
+        <Form1 />
       </div>
-    </>
+    </AuthLayout>
   );
 }
