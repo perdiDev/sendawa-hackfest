9bbde394833620ccfbe5a6b6dc50dc79cd8d69b8
Fayzul <fayzulhaq0708@gmail.com>
2024-01-11 22:08:16 +0800

    feat: add getMenuColletion

---
+++ src/firebase/firestore/getCollectionsData.ts
@@ -0,0 +1,26 @@
+import firebase_app from "../config";
+import { getFirestore, doc, getDoc, collection, getDocs } from "firebase/firestore";
+
+const db = getFirestore(firebase_app);
+
+export default async function getCollectionDataFirestore(
+  collection_ref: string,
+  id: string
+) {
+
+  const docRef = doc(db, collection_ref, id);
+  let result = null;
+  let error = null;
+
+  try {
+    const collectionVal = collection(docRef,'makanan')
+    const getValue = await getDocs(collectionVal)
+
+    result = getValue.docs.map(doc => ({...doc.data(), id:doc.id}))
+  } catch (e) {
+    error = e;
+  }
+
+
+  return { result, error };
+}
