54e337464fca8e347b95281d1f5c87cb7c50cad2
Fayzul <fayzulhaq0708@gmail.com>
2024-01-11 22:11:52 +0800

    chore: test

--- src/components/admin/MenuSection.tsx
+++ src/components/admin/MenuSection.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Menulist from "./Menulist";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 export default function MenuSection() {
   return (
     <div className="mt-10">
-      <h2 className="text-navy font-bold text-2xl pl-5">Menus</h2>
+      <h2 className="text-navy font-bold text-2xl pl-5">Menu</h2>
+      <Button>
+        <Link href={"new-menu"}>Tambahkan menu</Link>
+      </Button>
       <Menulist />
     </div>
   );
