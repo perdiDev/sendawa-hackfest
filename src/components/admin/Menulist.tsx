b0a0651ed85dd26a58be77d7b50ca0b2b0788262
Fayzul <fayzulhaq0708@gmail.com>
2024-01-11 22:11:25 +0800

    feat: connect to real menus

--- src/components/admin/Menulist.tsx
+++ src/components/admin/Menulist.tsx
@@ -1,16 +1,53 @@
-import React from 'react'
-import Menu from './Menu'
+"use client";
 
-const imageUrlEx = "https://awsimages.detik.net.id/community/media/visual/2021/04/22/5-makanan-enak-dari-indonesia-dan-malaysia-yang-terkenal-enak-5.jpeg?w=600&q=20"
+import Menu from "./Menu";
+import { useQuery } from "@tanstack/react-query";
+import { useAuthContext } from "@/context/AuthContext";
+import MenuListSkeleton from "../skeleton/MenuList";
+import getMenuCollection from "@/firebase/firestore/getCollectionsData";
 
 export default function Menulist() {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-3 p-5">
-      <Menu url={imageUrlEx} name="SATE PADANG" price={23000}/>
-      <Menu url={imageUrlEx} name="SATE PADANG" price={23000}/>
-      <Menu url={imageUrlEx} name="SATE PADANG" price={23000}/>
-      <Menu url={imageUrlEx} name="SATE PADANG" price={23000}/>
-      <Menu url={imageUrlEx} name="SATE PADANG" price={23000}/>
-    </div>
-  )
+  const { user } = useAuthContext() as { user: any };
+
+  const {
+    isLoading,
+    isError,
+    data: menus,
+    error,
+  } = useQuery({
+    queryKey: ["menu"],
+    queryFn: async () => {
+      const { result } = await getMenuCollection(
+        `${user.uid}`,
+        "menu"
+      );
+      return result;
+    },
+  });
+
+  if (isLoading) {
+    return <MenuListSkeleton />;
+  }
+  if (isError) {
+    console.log("Error", error);
+  }
+
+  if (menus) {
+    return (
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 p-5">
+        {menus.length == 0 ? (
+          <h1>Kosong</h1>
+        ) : (
+          menus.map((menu: any) => (
+            <Menu
+              url={menu.url}
+              name={menu.name}
+              price={menu.price}
+              key={menu.id}
+            />
+          ))
+        )}
+      </div>
+    );
+  }
 }
