2ab0439c78f03e56db194dd2ae517bae9a547e9c
Fayzul <fayzulhaq0708@gmail.com>
2024-01-12 18:09:40 +0800

    fix: fix typo to email

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -4,18 +4,18 @@ interface emailBodyInterface {
   to: string;
   subject?: string;
   body?: string;
-  name_brand?: string;
+  brand_name?: string;
 }
 
 export const sendEmail = async (emailBody: emailBodyInterface) => {
   const data = {
     subject:
       emailBody.subject ||
-      `🎉 Congratulations! Your Outlet is Now Officially Registered! Let's Shine Together! [${emailBody.name_brand} X Sendawa] 💼✨`,
+      `🎉 Congratulations! Your Outlet is Now Officially Registered! Let's Shine Together! [${emailBody.brand_name} X Sendawa] 💼✨`,
     to: emailBody.to,
     body:
       emailBody.body ||
-      `🌟 Thank You for Joining Us SENDAWA GANG! 🚀 Your registration means the world to us. Welcome aboard, and get ready for an amazing journey ahead! If you have any questions or need assistance, feel free to reach out. Let's make great things happen together! 🌈✨. \n\n\n Now, your outlet is still in the preparation phase 🛠️. We'll keep you posted with the latest updates about ${emailBody.name_brand}. It's time to add your delicious menus! Stay tuned...\nHelp WhatsApp: +62852299262611 (Fayzul)\n\n\nThank you for your cooperation! 🌟
+      `🌟 Thank You for Joining Us SENDAWA GANG! 🚀 Your registration means the world to us. Welcome aboard, and get ready for an amazing journey ahead! If you have any questions or need assistance, feel free to reach out. Let's make great things happen together! 🌈✨. \n\n\nNow, your outlet is still in the preparation phase 🛠️. We'll keep you posted with the latest updates about ${emailBody.brand_name}. It's time to add your delicious menus! Stay tuned...\n\nHelp WhatsApp: +62852299262611 (Fayzul)\n\n\nThank you for your cooperation! 🌟
     `,
   }
   try {
