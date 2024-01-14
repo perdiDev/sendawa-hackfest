a91d9cd3b6f17f45e8a7e3b95f2692ed5799e3a7
perdiDev <perdidev@gmail.com>
2024-01-14 22:58:30 +0800

    feat: metadata icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,15 @@ import "./globals.css";
 export const metadata = {
   title: "Sendawa",
   description: "Template to use Next.js with Firebase",
+  icons: {
+    icon: '/logo-sendawa.svg',
+    shortcut: '/logo-sendawa.svg',
+    apple: '/logo-sendawa.svg',
+    other: {
+      rel: '/logo-sendawa.svg',
+      url: '/logo-sendawa.svg',
+    },
+  },
 };
 
 export default function RootLayout({
